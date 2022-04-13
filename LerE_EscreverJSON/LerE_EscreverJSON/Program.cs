8a36772b09e782ce5e534d448495acd0e033d628
jucelio rodrigues <jucelioinfo@gmail.com>
2022-04-13 12:13:15 -0300

    first commit

---
+++ LerE_EscreverJSON/LerE_EscreverJSON/Program.cs
@@ -0,0 +1,57 @@
+﻿using Nancy.Json;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LerE_EscreverJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://ronnioliveira.wordpress.com/2016/11/05/como-ler-e-escrever-json-em-c/
+            //https://stackoverflow.com/questions/16921652/how-to-write-a-json-file-in-c
+            ReadAndWriteJsonFile();
+        }
+
+        public static void ReadAndWriteJsonFile()
+        {
+            try
+            {
+                var arquivoExterno = File.ReadAllText(@"E:\APLICAÇÕES E CURSOS\APPS GENÉRICOS\_LerE_EscreverJSON\Files\Source\arquivoJson.json");
+                var arquivoLido = JsonConvert.SerializeObject(arquivoExterno, Formatting.Indented);
+                List<Empresa> empresas = JsonConvert.DeserializeObject<List<Empresa>>(arquivoExterno);
+
+                //Exibe no console a leitura do arquivo JSON
+                Console.WriteLine("  ==  Resultado da leitura do arquivo JSON  == ");
+                Console.WriteLine("");
+                foreach (Empresa empresa in empresas)
+                {
+                    foreach (Funcionario func in empresa.Funcionarios)
+                    {
+                        Console.WriteLine(String.Format("{0} : {1} : {2}: {3}: {4}", empresa.Id, empresa.Nome, func.Id, func.Nome, func.Idade));
+                    }
+                }
+
+                //Faz a leitura e escrita do arquivo JSON
+                Console.WriteLine("");
+                Console.WriteLine("  ==  Leitura e escrita do arquivo JSON  == ");
+                Console.WriteLine("...");
+                List<Empresa> empresasSerialized = new List<Empresa>();
+
+                foreach (Empresa emp in empresas)
+                {
+                    empresasSerialized.Add(emp);
+                }
+                string jsonSerialize = JsonConvert.SerializeObject(empresasSerialized);
+                File.WriteAllText(@"E:\APLICAÇÕES E CURSOS\APPS GENÉRICOS\_LerE_EscreverJSON\Files\Destiny\teste.json", jsonSerialize);
+                Console.WriteLine("  ==  Leitura e escrita do arquivo JSON  finalizada== ");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# Console App de leitura e escrita de arquivos Json
+
+Aplicação console, feita em .Net core 5.0, usando conceitos sobre Serialize e Deserialize.
+
+## Pré requisitos
+
+1. [Visual Studio 2019](https://visualstudio.microsoft.com/pt-br/vs/)
+
+
+## Como baixar o código
+
+git clone https://github.com/JucelioAmaral/LeituraE_EscritaDeJSON.git
+
+
+## Como executar a aplicação?
+
+1. Abrir a solution;
+2. Executar o app pelo debug LerE_EscreverJSON.
+
+## Auxílio para uso e construção:
+
+1. [Tutorial de exemplo Como ler e escrever JSON em C#](https://ronnioliveira.wordpress.com/2016/11/05/como-ler-e-escrever-json-em-c/)
+2. [Tutorial de exemplo How to write a JSON file in C#?](https://stackoverflow.com/questions/16921652/how-to-write-a-json-file-in-c)
